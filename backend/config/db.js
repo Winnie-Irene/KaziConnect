19c29289c30f3d539c9fb7f2cd2849c0f504a256
Winnie-Irene <i.winnie@alustudent.com>
2025-11-28 00:07:50 +0300

    db host

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,16 +10,14 @@ if (connectionString) {
     pool = mysql.createPool(connectionString);
 } else {
     // Fallback to manual host/port
-    pool = mysql.createPool({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    });
+   const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    ssl: { rejectUnauthorized: true } 
+});
 }
 
 const promisePool = pool.promise();
