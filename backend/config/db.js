c5917cfc4ef16a07dd54285bff54ac2df5a7c47f
Winnie-Irene <i.winnie@alustudent.com>
2025-11-28 00:20:14 +0300

    db host

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connectionString = process.env.MYSQL_URL;
-
 let pool;
 
-if (connectionString) {
-    // Use Railway URL if provided
-    pool = mysql.createPool(connectionString);
+if (process.env.MYSQL_URL) {
+    pool = mysql.createPool(process.env.MYSQL_URL);
 } else {
-    // Fallback to manual host/port
-   const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    ssl: { rejectUnauthorized: true } 
-});
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT),
+        ssl: { rejectUnauthorized: true }
+    });
 }
 
 const promisePool = pool.promise();
