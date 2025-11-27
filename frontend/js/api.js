fdb0ed962724d2b5868ca501eb6a85ea27b2bd42
Winnie-Irene <i.winnie@alustudent.com>
2025-11-27 23:04:01 +0300

    changed public url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -3,7 +3,7 @@
  * Handles all HTTP requests to the backend API
  */
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://kazi-connect-phi.vercel.app/api';
 
 /**
  * Make HTTP request
